9e947eb4e0f3929d74d68d8da2a69918b531ec03
soasme <soasme@gmail.com>
2020-01-28 23:30:29 +1300

    extract move script for the player.

--- game/player/player.go
+++ game/player/player.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "move"
+  component: "/game/player/move.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
