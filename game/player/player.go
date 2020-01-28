e31393e82f10f7e8b4abc794affab40d572ffe17
soasme <soasme@gmail.com>
2020-01-28 21:48:24 +1300

    open box if taking action.

--- game/player/player.go
+++ game/player/player.go
@@ -41,7 +41,7 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"default\"\n"
+  "group: \"player\"\n"
   "mask: \"rocks\"\n"
   "mask: \"box\"\n"
   "embedded_collision_shape {\n"
