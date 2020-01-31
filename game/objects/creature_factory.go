74620761b544b1e694428a5308ad56d143b78d0d
soasme <soasme@gmail.com>
2020-02-01 11:28:31 +1300

    add creature factory.

---
+++ game/objects/creature_factory.go
@@ -0,0 +1,33 @@
+components {
+  id: "script"
+  component: "/game/objects/creature_factory.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "creaturefactory"
+  type: "factory"
+  data: "prototype: \"/game/objects/creature_factory.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
