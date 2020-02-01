d41916e84d40777cc99870d40c0c894a7699ca70
soasme <soasme@gmail.com>
2020-02-02 08:28:37 +1300

    clean code.

--- game/levels/level_01.go
+++ game/levels/level_01.go
@@ -28,29 +28,3 @@ components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "collisionobject"
-  type: "collisionobject"
-  data: "collision_shape: \"/game/levels/level_01.tilemap\"\n"
-  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
-  "mass: 0.0\n"
-  "friction: 0.1\n"
-  "restitution: 0.5\n"
-  "group: \"default\"\n"
-  "mask: \"default\"\n"
-  "linear_damping: 0.0\n"
-  "angular_damping: 0.0\n"
-  "locked_rotation: false\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
